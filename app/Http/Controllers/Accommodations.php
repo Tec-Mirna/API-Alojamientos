6231d81b820cfbfcecd43ddaa1a7f2b4368eda56
Mirna <mirnarecinos382@gmail.com>
2024-05-15 20:12:43 -0300

    Endpoints :sparkles:

--- app/Http/Controllers/Accommodations.php
+++ app/Http/Controllers/Accommodations.php
@@ -13,6 +13,42 @@ class Accommodations extends Controller
 
         ]);
     }
+
+    /* Obtner todos */
+    public function accomodations(){
+        return response()->json([
+            'message' => 'Get all accomodations'
+        ]);
+    }
+
+
+    /* Traer por id */
+    public function accomodation($id){
+        return response()->json([
+            'message' => 'Get accomodation with id ' . $id
+        ]);
+    }
+
+/* POST */
+    public function createAccomodation(){
+        return response()->json([
+            'message' => 'New accomodation created'
+        ]);
+    }
+
+    /* PUT */
+    public function updateAccomodation($id){
+        return response()->json([
+            'message' => 'Update accomodation with id' . $id
+        ]);
+    }
+
+    /* DELETE */
+    public function deleteAccomodation($id){
+        return response()->json([
+            'message' => 'Delete accomodation with id' . $id
+        ]);
+    }
         
         
        
--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,18 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/test', [Accommodations::class, 'test']);
+
+/* Traer todos los acommodations */
+Route::get('/accomodation/all', [Accommodations::class, 'accomodations']);
+
+/* Traer por id */
+Route::get('/accomodation/{id}', [Accommodations::class, 'accomodation']);
+
+/* Método POST */
+Route::post('/accomodation', [Accommodations::class, 'createAccomodation']);
+
+/* Método edit */
+Route::put('/accomodation/{id}', [Accommodations::class, 'updateAccomodation']);
+
+/* Método de eliminar */
+Route::delete('/accomodation/{id}', [Accommodations::class, 'deleteAccomodation']);
