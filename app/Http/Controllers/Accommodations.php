5f0d7139af4842140bbc5d3c835857d21dc81d89
Mirna <mirnarecinos382@gmail.com>
2024-05-16 21:37:25 -0300

    Controlador Accommodations para trabajar endpoints :sparkles:

--- app/Http/Controllers/Accommodations.php
+++ app/Http/Controllers/Accommodations.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Accomodation;
 
 class Accommodations extends Controller
 {
@@ -16,41 +17,80 @@ class Accommodations extends Controller
 
     /* Obtner todos */
     public function accomodations(){
+
+        $accomodations = Accommodation::all(); /* SELECT * FROM [nombre_tabla] */
         return response()->json([
-            'message' => 'Get all accomodations'
+            'status' => true,
+            'message' => 'Get all accomodations successfully',
+            'data' => $accomodations
         ]);
     }
 
 
     /* Traer por id */
     public function accomodation($id){
+
+        $accomodation = Accomodation::find($id); /* Obtener 1 por id */
+
+        /* Validaciones  vacía */
+        if(!$accomodation){
+            return response()->json([
+                'status' => false,
+                'message' => 'Accomodation with id ' . $id . 'Not found',
+                'data' => $accomodation
+            ], 404); /* Responde mal */
+        }
+
+
         return response()->json([
-            'message' => 'Get accomodation with id ' . $id
-        ]);
+            'status' => true,
+            'message' => 'Get accomodation with id ' . $id,
+            'data' => $accomodation
+        ], 200); /* Responde bien */
     }
 
 /* POST */
-    public function createAccomodation(){
+    public function createAccomodation(Request $request){ 
+        /* Donde el nombre sea igual a request name */
+        $accomodationDatabase = Accomodation::where('name', $request->name)->first();
+       
+        if($accomodationDatabase){
+            return response()->json([
+                'status' =>  false,
+                'message' => 'Accomodation alredy exist',
+                'data' => 'NOT data'
+            ], 409); 
+        }
+      
+        $accomodation = Accomodation::create($request->all()); /* INSERT INTO */
         return response()->json([
-            'message' => 'New accomodation created'
-        ]);
+            'status' =>  true,
+            'message' => 'New ccomodation created',
+            'data' => $accomodation
+        ], 201); 
+
     }
 
     /* PUT */
-    public function updateAccomodation($id){
+    public function updateAccomodation( Request $request, $id){
+
+        $accomodation = Accomodation::update($request->all());
+
         return response()->json([
-            'message' => 'Update accomodation with id' . $id
+            'status' =>  true,
+            'message' => 'Update accomodation with id',
+            'data' => $accomodation
         ]);
     }
 
     /* DELETE */
     public function deleteAccomodation($id){
+
+        $accomodation = Accomodation::delete($id);
+
         return response()->json([
-            'message' => 'Delete accomodation with id' . $id
+            'message' => 'Delete accomodation with id' . $id . 'deleted.'
         ]);
-    }
-        
-        
-       
+    }    
        
 }
